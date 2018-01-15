99de7552df10f7589fbbab3327bc2d440659473c
Unknown <huy.jeanh@outlook.com>
2018-01-15 01:32:35 +0100

    Add Hello World

--- rt_hack/__init__.py
+++ rt_hack/__init__.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for RT_Hack."""
+"""RT_Hack
+
+Usage:
+  rt_hack
+  rt_hack (-h | --help)
+  rt_hack --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
 
 __author__ = """Jean-Hieu HUYNH"""
 __email__ = 'jean-hieu.huynh@ensea.fr'
 __version__ = '0.0.1'
+
+from docopt import docopt
+from scapy.all import *
+
+IP = []
+"""List of reachable IP addresses"""
+
+def main():
+	"""Main function that acts as a CLI"""
+	print('Hello World')
 
 if __name__ == '__main__':
     import doctest
